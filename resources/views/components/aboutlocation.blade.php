e430603367687520ef81503d3549e1ec2b44b0f1
= <=>
2025-06-11 10:16:45 -0500

    update hours

--- resources/views/components/aboutlocation.blade.php
+++ resources/views/components/aboutlocation.blade.php
@@ -25,7 +25,8 @@
                         <i class="bi bi-caret-right-fill"></i> HOURS 
                     </p>
                     <p class="leadsmaller fw-normal mb-5">    
-                        <i class="bi bi-lightning-charge-fill green"></i> Tues. &#8211; Fri. 10 a.m. &#8211; 7 p.m.<br />
+                        <i class="bi bi-lightning-charge-fill green"></i> Tues. &amp; Wed. 10 a.m. &#8211; 5:30 p.m.<br />
+                        <i class="bi bi-lightning-charge-fill green"></i> Thurs. &amp; Fri. 10 a.m. &#8211; 7 p.m.<br />
                         <i class="bi bi-lightning-charge-fill green"></i> Sat. &amp; Sun. 9 a.m. &#8211; 5 p.m.<br />
                         &nbsp;&nbsp;&nbsp;<em id="location">*Closed on Mon.</em>
                     </p>
@@ -39,7 +40,7 @@
                         <i class="bi bi-lightning-charge-fill green"></i> <a href="https://www.google.ca/maps/place/Twin+Beans+Coffee/@41.4824305,-87.4719419,17z/data=!4m15!1m8!3m7!1s0x8811e3ef979e6281:0xcae7b45788b5780b!2s2206+US-41,+Schererville,+IN+46375!3b1!8m2!3d41.4824305!4d-87.4719419!16s%2Fg%2F11c4rpgwt8!3m5!1s0x8811e39a44637939:0x1221c628dc7f028a!8m2!3d41.4824708!4d-87.4721188!16s%2Fg%2F11vjdvnqm_?entry=ttu"><u>2206 US Highway 41, Schererville,&nbsp;Indiana, 46375</u></a><br />
                         <span class="green">A HIDDEN GEM&#8212;Pull in to the <em>Hearts In Motion </em>plaza, drive to the back, and look for the big sign!</span><br />
                         <i class="bi bi-lightning-charge-fill green"></i> (219) 463-1044<br />
-                        twinbeansllc@gmail.com                            </p>
+                        <i class="bi bi-lightning-charge-fill green"></i> twinbeansllc@gmail.com                            </p>
 
                     <div class="fb-share-button" data-href="https://www.twinbeansnwi.com/" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.twinbeansnwi.com%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
                     
